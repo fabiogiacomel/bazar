27dfefb0cf3278dd151f49d217a5f49a73e85202
fabiogiacomel <fabioceep@gmail.com>
2021-08-15 11:02:55 -0300

    0.01

--- js/lib.js
+++ js/lib.js
@@ -7,4 +7,13 @@ function loadDoc() {
     xhttp.send();
   }
 
-  
+  function inserirPedido() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "db/inserirPedido.php");
+    xhttp.send();
+  }
+
+ 
