b172e3bce3fcaf38460361b7630a67669418f687
fabiogiacomel <fabioceep@gmail.com>
2021-08-15 14:40:36 -0300

    0.06

--- db/selecionarProdutos.php
+++ db/selecionarProdutos.php
@@ -5,7 +5,7 @@
 
 <?php
 echo "<table style='border: solid 1px black;'>";
- echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th><th>Firstname</th></tr>";
+ echo "<tr><th>Id</th><th>Produto</th><th>img</th><th>Descricao</th><th>Valor R$</th></tr>";
 
 class TableRows extends RecursiveIteratorIterator {
     function __construct($it) {
