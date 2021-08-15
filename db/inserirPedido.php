73a2f7d8bf824cd685490ed65681e0648f15d854
fabiogiacomel <fabioceep@gmail.com>
2021-08-15 10:53:08 -0300

    separacao do javascript

---
+++ db/inserirPedido.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "u224722929_bazar";
+$password = "cA*di&1lVkZp";
+$dbname = "u224722929_bazar";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// prepare and bind
+$stmt = $conn->prepare("INSERT INTO pedido (idSessao,idProduto,ip) VALUES (?, ?, ?)");
+$stmt->bind_param("iis",$idSessao, $idProduto, $ip);
+
+// set parameters and execute
+$idSessao = 123456;
+$idProduto = 10;
+$ip="192.168.1.100";
+$stmt->execute();
+
+echo "Produto inserio no Pedido";
+
+$stmt->close();
+$conn->close();
+?>
