1d776b9245b55401be7ab376272185600ac2b4e4
fabiogiacomel <fabioceep@gmail.com>
2021-08-15 01:12:16 -0300

    teste bd

--- db/inserir.php
+++ db/inserir.php
@@ -1,24 +1,29 @@
 <?php
 $servername = "localhost";
-$username = "";
-$password = "";
-$dbname = "";
+$username = "u224722929_bazar";
+$password = "cA*di&1lVkZp";
+$dbname = "u224722929_bazar";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO usuario (email, senha)
-VALUES ('John', 'john@example.com')";
+// prepare and bind
+$stmt = $conn->prepare("INSERT INTO usuario (email,senha) VALUES (?, ?)");
+$stmt->bind_param("ss", $email, $senha);
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
+// set parameters and execute
+$email = "john@example.com";
+$senha = sha1("John");
+$stmt->execute();
 
+
+echo "New records created successfully";
+
+$stmt->close();
 $conn->close();
 ?>
