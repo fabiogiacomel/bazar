e080e345463198ecd7a755ac741115c31c0f9fd8
fabiogiacomel <fabioceep@gmail.com>
2021-08-15 14:36:25 -0300

    0.07

--- index.php
+++ index.php
@@ -83,18 +83,7 @@ session_start();
       </div>
 
 
-      <div class="col-sm-4">
-        <h2>FONTE ATX</h2>
-        <h5>Várias Marcas</h5>
-        <img src="ofertados/03.png" class="mx-auto d-block img-fluid">
-        <p>Quantidade: 10</p>
-        <h3>R$ 5,00</h3>
-        <ul class="nav nav-pills flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" onclick="document.getElementById('demo').innerHTML=Date()">Eu quero!</a>
-          </li>
-        </ul>
-      </div>
+
 
 
     </div>
@@ -104,7 +93,7 @@ session_start();
 
 
   <div class="jumbotron text-center bg-success text-white" style="margin-bottom:0">
-    <p>Unidos pelo meio ambiente!</p>
+  <?php require 'mensagens/02.php';?>
   </div>
 
   <script src="js/lib.js"></script>
