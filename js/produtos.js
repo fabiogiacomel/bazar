1df01f4d229d26cf97b3f5be160d09d123b85e6e
fabiogiacomel <fabioceep@gmail.com>
2021-08-15 22:18:24 -0300

    0.09

--- js/produtos.js
+++ js/produtos.js
@@ -2,5 +2,5 @@ var produtosObj = {
     "records":[
     {"id":"1","titulo":"Monitor","img":"01.png","descricao":" Monitores de várias marcas e modelos","preco":"R$ 25,00"},
     {"id":"2","titulo":"HD","img":"02.png","descricao":" Capacidade 80 GB","preco":"R$ 10,00"},
-    {"id":"3","titulo":"Fonte ATX","img":"03.png","descricao":"Fonte utilizada no PC","preco":"R$ 5,00"},
+    {"id":"3","titulo":"Fonte ATX","img":"03.png","descricao":"Fonte utilizada no PC","preco":"R$ 5,00"}
 ]};
