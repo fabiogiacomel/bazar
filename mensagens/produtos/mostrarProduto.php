bb5e4e8a64ebf51748913ba7538278e182c2a6f7
fabiogiacomel <fabioceep@gmail.com>
2021-08-15 23:11:04 -0300

    0.10

--- mensagens/produtos/mostrarProduto.php
+++ mensagens/produtos/mostrarProduto.php
@@ -2,7 +2,7 @@
     <div appml-repeat="records">    
         <h2>{{titulo}}</h2>
         <h5>{{descricao}}</h5>
-        <img src={{img}} class="mx-auto d-block img-fluid">;
+        <img src="{{img}}" class="mx-auto d-block img-fluid">
         <p>Quantidade: 10</p>
         <h3>R$ 5,00</h3>
         <ul class="nav nav-pills flex-column">
