c33de2f634824facd0dc907fdd0de8647b417236
fabiogiacomel <fabioceep@gmail.com>
2021-08-15 22:32:55 -0300

    0.09

--- index.php
+++ index.php
@@ -13,14 +13,7 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   <script src="https://www.w3schools.com/appml/2.0.3/appml.js"></script>
 
-<script>
-    var produtosObj = {
-    "records":[
-    {"id":"1","titulo":"Monitor","img":"01.png","descricao":"Monitores de várias marcas e modelos","preco":"R$ 25,00"},
-    {"id":"2","titulo":"HD","img":"02.png","descricao":"Capacidade 80 GB","preco":"R$ 10,00"},
-    {"id":"3","titulo":"Fonte ATX","img":"03.png","descricao":"Fonte utilizada no PC","preco":"R$ 5,00"}
-]};
-</script>
+
 
 <style>
     .fakeimg {
@@ -112,7 +105,7 @@
 
   <script src="js/lib.js"></script>
   <script src="js/inserirPedido.js"></script>
-  
+
 </body>
 
 </html>
