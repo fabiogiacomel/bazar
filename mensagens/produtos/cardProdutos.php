6df17b4324c4f684849b31b9c7de85798b0f2c94
fabiogiacomel <fabioceep@gmail.com>
2021-08-17 00:18:37 -0300

    0.18

--- mensagens/produtos/cardProdutos.php
+++ mensagens/produtos/cardProdutos.php
@@ -1,7 +1,7 @@
-<div class="card" appml-data="appml/produtos.js">
-    <div appml-repeat="records">  
+<div class="card row" appml-data="appml/produtos.js">
+    <div class="col" appml-repeat="records">  
     <img class="card-img-top img-thumbnail mx-auto d-block img-fluid" src="ofertados/{{img}}" alt="Card image" style="width:100%">
-    <div class="card-body">
+    <div class="card-body text-center">
       <h4 class="card-title">{{titulo}}</h4>
       <p class="card-text">{{descricao}}</p>
       <div class="alert alert-success">
