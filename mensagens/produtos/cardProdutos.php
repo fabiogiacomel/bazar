b5a382413772f657156b3ece1f521d5d00b30727
fabiogiacomel <fabioceep@gmail.com>
2021-08-17 00:13:28 -0300

    0.17

--- mensagens/produtos/cardProdutos.php
+++ mensagens/produtos/cardProdutos.php
@@ -1,13 +1,13 @@
 <div class="card" style="width:400px" appml-data="appml/produtos.js">
     <div appml-repeat="records">  
-    <img class="card-img-top" src="ofertados/{{img}}" alt="Card image" style="width:100%">
+    <img class="card-img-top img-thumbnail mx-auto d-block img-fluid" src="ofertados/{{img}}" alt="Card image" style="width:100%">
     <div class="card-body">
       <h4 class="card-title">{{titulo}}</h4>
       <p class="card-text">{{descricao}}</p>
       <div class="alert alert-success">
           <strong><h3>{{preco}}</h3></strong>
         </div>
-      <a href="#" class="btn btn-primary">Reservar apartir de sexta</a>
+      <a href="#" class="btn btn-primary">Reservar (Começa na sexta)</a>
     </div>
   </div>
 </div>
