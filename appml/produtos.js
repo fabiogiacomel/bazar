0abcecdb8b7d48b61aa1fd25f1d6d94fbdf1f61d
fabiogiacomel <fabioceep@gmail.com>
2021-08-17 01:27:16 -0300

    0.22

--- appml/produtos.js
+++ appml/produtos.js
@@ -7,6 +7,17 @@
     {"id":"5","titulo":"Memória RAM DDR 2","img":"05.png","descricao":"Memória DDR2 256 MB","qtd":"10","preco":"R$ 5,00"},
     {"id":"6","titulo":"Cooler 775","img":"06.png","descricao":"Usado deve ser utilizado com pasta termica","qtd":"3","preco":"R$ 15,00"},
     {"id":"7","titulo":"Antena Ubiquiti(o Par)","img":"07.png","descricao":"Antenas doadas por um provedor","qtd":"10","preco":"R$ 5,00"},
-    {"id":"8","titulo":"Cabo de Força (Padrão Novo)","img":"08.png","descricao":"Cabo para ligar a fonte do computador à rede elétrica","qtd":"10","preco":"R$ 2,00"}
+    {"id":"8","titulo":"Cabo de Força (Padrão Novo)","img":"08.png","descricao":"Cabo para ligar a fonte do computador à rede elétrica","qtd":"10","preco":"R$ 2,00"},
+    {"id":"9","titulo":"Placa De Video AGP","img":"09.png","descricao":"Placa de Vídeo geralmente utilizada em P4 (Roda no máximo GTA San Andres)","qtd":"5","preco":"R$ 2,00"},
+    {"id":"10","titulo":"Ventilador de Fonte (Para projetos)","img":"10.png","descricao":"Esse ventilador pode ser utilizado para vários projetos ele é retirado de fontes ATX","qtd":"5","preco":"R$ 1,00"},
+    {"id":"11","titulo":"Processador Celeron 775","img":"11.png","descricao":"Processador Funcionando","qtd":"3","preco":"R$ 5,00"},
+    {"id":"12","titulo":"Teclado Usado","img":"12.png","descricao":"Teclado usado funcionando","qtd":"3","preco":"R$ 5,00"},
+    {"id":"13","titulo":"Mouse Usado","img":"13.png","descricao":"Mouse usado funcinando","qtd":"3","preco":"R$ 5,00"},
+    {"id":"14","titulo":"Roteador TP-Link","img":"14.png","descricao":"Roteadores TP-link usado","qtd":"3","preco":"R$ 5,00"},
+    {"id":"15","titulo":"Roteador Arlive","img":"15.png","descricao":"Roteadores Arlive usado","qtd":"3","preco":"R$ 10,00"},
+    {"id":"16","titulo":"Cabo de Energia (Notebook)","img":"16.png","descricao":"Cabo utilizado em fontes de notebooks","qtd":"3","preco":"R$ 2,00"},
+    {"id":"17","titulo":"Gabinetes ATX (Sem peças)","img":"17.png","descricao":"Vários modelos e várias cores na hora de retirar você pode escolher o modelo e a cor ","qtd":"3","preco":"R$ 5,00"},
+    {"id":"18","titulo":"Gabinetes ATX (Com peças sem testar)","img":"17.png","descricao":"Computador doado pela comunidade que ainda não foi testado algumas peças podem estar com defeito","qtd":"3","preco":"R$ 30,00"}
+
 ]
 }
