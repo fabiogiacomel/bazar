c0b1834d84f4a611f5f32cb135b78dcc36f369fd
fabiogiacomel <fabioceep@gmail.com>
2021-08-17 16:05:43 -0300

    0.27

--- mensagens/m01.php
+++ mensagens/m01.php
@@ -1,5 +1,6 @@
+</br></br>
 <div class="table table-bordered justify-content-center align-items-center">   
         <div class="text-center">
         <img src="mensagens/01.png" class="img-thumbnail mx-auto d-block img-fluid">
     </div>
-</div></br>
+</div></br></br>
