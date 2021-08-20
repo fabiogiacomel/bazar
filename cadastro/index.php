258e0c864eb18afceefaec66cffa52d5c2677129
fabiogiacomel <fabioceep@gmail.com>
2021-08-20 09:14:57 -0300

    teste

--- cadastro/index.php
+++ cadastro/index.php
@@ -2,20 +2,44 @@
 <body>
 
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Name: <input type="text" name="fname">
+  Nome:  <input type="text" name="fnome">
+  Email: <input type="text" name="femail">
+  Fone:  <input type="text" name="ffone">
   <input type="submit">
 </form>
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // collect value of input field
-  $name = $_POST['fname'];
+  $nome = $_POST['fnome'];
   if (empty($name)) {
-    echo "Name is empty";
+    echo "Nome em branco";
   } else {
-    echo $name;
+    echo $nome;
   }
 }
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // collect value of input field
+  $email = $_POST['femail'];
+  if (empty($email)) {
+    echo "Email em Branco";
+  } else {
+    echo $email;
+  }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // collect value of input field
+  $fone = $_POST['ffone'];
+  if (empty($fone)) {
+    echo "Fone em Branco";
+  } else {
+    echo $fone;
+  }
+}
+
 ?>
 
 </body>
