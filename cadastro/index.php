63fb552bd7df4687b43cb49ca4da768933ec4033
fabiogiacomel <fabioceep@gmail.com>
2021-08-21 10:09:36 -0300

    1.1

--- cadastro/index.php
+++ cadastro/index.php
@@ -61,8 +61,7 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO  (nome)
-  VALUES ('teste')";
+  $sql = "INSERT INTO  usuario (nome) VALUES ('teste')";
   // use exec() because no results are returned
   $conn->exec($sql);
   echo "New record created successfully";
