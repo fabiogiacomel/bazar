1078ab0e53f2b77db410d8e346800bde833645f9
fabiogiacomel <fabioceep@gmail.com>
2021-08-21 15:29:12 -0300

    login

--- cadastro/listaUsuario.php
+++ cadastro/listaUsuario.php
@@ -10,7 +10,7 @@ $senha = sha1('fabio');
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT email FROM usuario WHERE email=$login");
+    $stmt = $conn->prepare("SELECT email FROM usuario WHERE email==$login");
     $stmt->execute();
 
     // set the resulting array to associative
