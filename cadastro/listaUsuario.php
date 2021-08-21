0ea6ceb1266880c1114bb1a4b4cd70c2ce4f1d7e
fabiogiacomel <fabioceep@gmail.com>
2021-08-21 15:18:15 -0300

    login

---
+++ cadastro/listaUsuario.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "u224722929_bazar";
+$password = "cA*di&1lVkZp";
+$dbname = "u224722929_bazar";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT nome FROM usuario");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    echo $resutl;
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+?>
