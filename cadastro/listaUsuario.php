814f5856fb53f25faf97d5aca2f826e82a9a61a8
fabiogiacomel <fabioceep@gmail.com>
2021-08-21 15:23:34 -0300

    login

--- cadastro/listaUsuario.php
+++ cadastro/listaUsuario.php
@@ -7,12 +7,21 @@ $dbname = "u224722929_bazar";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT nome FROM usuario");
+    $stmt = $conn->prepare("SELECT email FROM usuario");
     $stmt->execute();
 
     // set the resulting array to associative
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
     echo $result;
+    if ($result) != 1) {
+        // Mensagem de erro quando os dados são inválidos e/ou o usuário não foi encontrado
+        echo "Login inválido!"; exit;
+    } else {
+        // Salva os dados encontados na variável $resultado
+        echo "login valido";
+    }
+
+
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
