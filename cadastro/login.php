85d9c1ee9a24d71aa9eb6977bdb94d6bbed470f8
fabiogiacomel <fabioceep@gmail.com>
2021-08-21 22:34:02 -0300

    login

--- cadastro/login.php
+++ cadastro/login.php
@@ -26,19 +26,19 @@ if (!empty($_POST) and (empty($_POST['login']) or empty($_POST['senha']))) {
 
         $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
         //print_r($stmt->fetchAll());
-        $dados[][] = $stmt->fetchAll();
-
-        echo $dados[0][0];
+        //$dados = $stmt->fetchAll();
+        $dados = $stmt->fetchColumn(1);
+        echo $dados;
    
     // Se a sessão não existir, inicia uma
         if (!isset($_SESSION)) session_start();
-
+/*
         // Salva os dados encontrados na sessão
         $_SESSION["UsuarioID"] = $dados[0][0];
         $_SESSION["UsuarioNome"] = $dados[0][1];
        // $_SESSION['UsuarioNivel'] = $resultado['nivel'];
         $_SESSION["UsuarioEmail"] = $dados[0][2];
-  
+  */
         // Redireciona o visitante
        //     header("Location: http://bazar.infoceep.com.br"); exit;
     } catch (PDOException $e) {
