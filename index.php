d10660f59cddd65a0e0c53051213769549e16753
fabiogiacomel <fabioceep@gmail.com>
2021-08-21 22:22:10 -0300

    loja

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
   // A sessão precisa ser iniciada em cada página diferente
-  if (!isset($_SESSION)) session_start();
+ // if (!isset($_SESSION)) session_start();
 
   // Verifica se não há a variável da sessão que identifica o usuário
   if (!isset($_SESSION['UsuarioID'])) {
