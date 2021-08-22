bac3e1477480a4b20cb01d47f77d4a843b04ed46
fabiogiacomel <fabioceep@gmail.com>
2021-08-21 23:02:50 -0300

    login

--- cadastro/login.php
+++ cadastro/login.php
@@ -44,7 +44,8 @@ if (!empty($_POST) and (empty($_POST['login']) or empty($_POST['senha']))) {
         $_SESSION["UsuarioNome"] = $stmt->fetchColumn(1);
        // $_SESSION['UsuarioNivel'] = $resultado['nivel'];
         $_SESSION["UsuarioEmail"] = $stmt->fetchColumn(2);
-
+        
+        echo $_SESSION["UsuarioID"], $_SESSION["UsuarioNome"], $_SESSION["UsuarioEmail"];
         // Redireciona o visitante
        //     header("Location: http://bazar.infoceep.com.br"); exit;
     } catch (PDOException $e) {
