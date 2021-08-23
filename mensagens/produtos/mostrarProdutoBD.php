94e269090460cc2456fb97a514c375f1f2eef90c
fabiogiacomel <fabioceep@gmail.com>
2021-08-22 23:51:29 -0300

    lista produtos

--- mensagens/produtos/mostrarProdutoBD.php
+++ mensagens/produtos/mostrarProdutoBD.php
@@ -6,6 +6,7 @@ class TableRows extends RecursiveIteratorIterator {
   }
 
   function current() {
+
     return " " . parent::current(). " ";
   }
 
@@ -31,41 +32,44 @@ try {
 
   // set the resulting array to associative
   $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  
+  $i=0;
+
   foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-    echo $v;
+    if($i == 0) { 
+      echo '<h2>'.$v.'</h2></br>';
+    }
+
+    if($i == 0) { 
+      echo '<img src="ofertados/'.$v.'" class="img-thumbnail mx-auto d-block img-fluid">';
+    }
+
+    if($i == 0) { 
+      echo '<p>Quantidade: '.$v.'</p>';
+    }
+
+    if($i == 0) { 
+      echo '<h5>'.$v.'</h5>';
+    }
+
+    if($i == 0) { 
+      echo '<div class="alert alert-success"><strong><h3>'.$v.'</h3></strong></div>';
+    }
+
+    if($i == 0) { 
+
+      echo '<ul class="nav nav-pills flex-column"><li class="nav-item"><a href="?acao=incluir&cod='.$v.'" class="btn btn-primary">Reservar (Começa na sexta)</a></li></ul>';
+      echo '</div></br></div>';
+    }
+
+    $i++;
   }
 } catch(PDOException $e) {
   echo "Error: " . $e->getMessage();
 }
 $conn = null;
-echo "</div>";
 ?>
 
-
-
-
-
-
     
-      </br>
-      <h2>{{titulo}}</h2>
-      <img src="ofertados/{{img}}" class="img-thumbnail mx-auto d-block img-fluid">
-      <p>Quantidade: {{qtd}}</p>
-      <h5>{{descricao}}</h5>
-
-      <div class="alert alert-success">
-        <strong>
-          <h3>{{preco}}</h3>
-        </strong>
-      </div>
-      <ul class="nav nav-pills flex-column">
-        <li class="nav-item">
-          <a href="?acao=incluir&cod={{id}}" class="btn btn-primary" onclick="addCarrinho({{id}})">Reservar (Começa na sexta)</a>
-          <? # Botao desativado
-          #<a class="nav-link active" onclick="document.getElementById('demo').innerHTML=Date()">Eu quero!</a>
-          ?>
-        </li>
-      </ul>
-    </div>
-    </br>
-  </div>
+    
+      
