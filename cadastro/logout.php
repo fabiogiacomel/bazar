1bb87d7fc017b275d8eb0ab960e70286b795e161
fabiogiacomel <fabioceep@gmail.com>
2021-08-23 02:09:51 -0300

    erro logout

--- cadastro/logout.php
+++ cadastro/logout.php
@@ -1,5 +1,5 @@
 <?php
       session_start(); // Inicia a sessão
       session_destroy(); // Destrói a sessão limpando todos os valores salvos
-      header("Location: cadastro/login.php"); exit; // Redireciona o visitante
+      header("Location: ../"); exit; // Redireciona o visitante
   ?>
