839d79d4eff4cb8afc2aa6fe7bb8297543dae379
fabiogiacomel <fabioceep@gmail.com>
2021-08-23 00:36:39 -0300

    lista

--- mensagens/produtos/mostrarProdutoBD.php
+++ mensagens/produtos/mostrarProdutoBD.php
@@ -1,20 +1,25 @@
 <?php
 
-class TableRows extends RecursiveIteratorIterator {
-  function __construct($it) {
+class TableRows extends RecursiveIteratorIterator
+{
+  function __construct($it)
+  {
     parent::__construct($it, self::LEAVES_ONLY);
   }
 
-  function current() {
+  function current()
+  {
 
     return parent::current();
   }
 
-  function beginChildren() {
+  function beginChildren()
+  {
     //echo "<div>";
   }
 
-  function endChildren() {
+  function endChildren()
+  {
     //echo "</div>" . "\n";
   }
 }
@@ -32,44 +37,46 @@ try {
 
   // set the resulting array to associative
   $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-  
-  $i=0;
+
+  $i = 0;
   $id = 0;
-  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-    if($i == 0) {
-      $id = $v; 
-//      echo '<h2>'.$v.'</h2></br>';
+  foreach (new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k => $v) {
+    if ($i == 0) {
+      $id = $v;
+      //      echo '<h2>'.$v.'</h2></br>';
     }
-    if($i == 1) {
+    if ($i == 1) {
       echo '<div class="table table-bordered justify-content-center align-items-center">';
       echo '<div class="text-center">';
-      echo '<h2>'.$v.'</h2></br>';
+      echo '<h2>' . $v . '</h2></br>';
     }
 
-    if($i == 2) { 
-      echo '<img src="ofertados/'.$v.'" class="img-thumbnail mx-auto d-block img-fluid">';
+    if ($i == 2) {
+      echo '<img src="ofertados/' . $v . '" class="img-thumbnail mx-auto d-block img-fluid">';
     }
 
-    if($i == 3) { 
-      echo '<h5>'.$v.'</h5>';
-     
+    if ($i == 3) {
+      echo '<h5>' . $v . '</h5>';
     }
 
-    if($i == 4) { 
-      echo '<p>Quantidade: '.$v.'</p>';
+    if ($i == 4) {
+      echo '<div class="alert alert-success"><strong><h3>' . $v . '</h3></strong></div>';
     }
 
-    if($i == 5) { 
-      echo '<div class="alert alert-success"><strong><h3>'.$v.'</h3></strong></div>';
-      echo '<ul class="nav nav-pills flex-column"><li class="nav-item"><a href="?acao=incluir&cod='.$id.'" class="btn btn-primary">Reservar (Começa na sexta)</a></li></ul>';
+    if ($i == 5) {
+      echo '<p>Quantidade: ' . $v . '</p>';
+      echo '<ul class="nav nav-pills flex-column"><li class="nav-item"><a href="?acao=incluir&cod=' . $id . '" class="btn btn-primary">Reservar (Começa na sexta)</a></li></ul>';
       echo '</div></br></div>';
     }
 
 
-    if($i == 5) {$i = 0;} else {$i++;}; 
-    
+    if ($i == 5) {
+      $i = 0;
+    } else {
+      $i++;
+    };
   }
-} catch(PDOException $e) {
+} catch (PDOException $e) {
   echo "Error: " . $e->getMessage();
 }
 $conn = null;
